ce9554a1cebeef5e63bd8086ecfacc495cb37edc
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-08-08 14:08:53 -0700

    Created multiple functions for each part of the app. Gif buttons can be added Gifs can be generated. Gifs can be toggled. Issues with CSS button spacing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,67 @@
 $(function(){
 	$("#add-btn").click(function(){
 		addNewGifBtn($("#search-value").val());
-		console.log($("#search-value").val())
+		console.log($("#search-value").val());
+		return false;
 	})
+	// $('#add-btn').click(function() {
+	// 	generateGifs("Money");	
+	// });
+
 	function addNewGifBtn(searchValue){
 		var btn = $('<button type="button" class="btn btn-success">' + searchValue + '</button>');
 		$("#gif-searches").append(btn);
+
+		btn.click(function() {
+			searchValue = $(this).text();
+			generateGifs(searchValue);
+		});
 	}
 
-	function generateGifs(){
+	function generateGifs(searchValue){
+		var giphyLink = "http://api.giphy.com/v1/gifs/search?q=" + searchValue + "&api_key=dc6zaTOxFJmzC&limit=20&offset=0";
+		var gifArray;
 
+		$.getJSON(giphyLink, function(json) {
+			gifArray = json;
+
+			$('#results').empty();
+
+			for(i=0; i<gifArray.data.length; i++){
+				appendGif(gifArray.data[i]);
+			}
+
+		});
+		console.log(gifArray);
 	}
 
-	function appendGif()
+	function appendGif(giphyUrl)
 	{
-		var btn = $('<button type="button" class="btn btn-success">' + searchValue + '</button>');
+		var newContainer = $('<div>');
+		newContainer.attr('id', 'img-container');
 
+		var newImage = $('<img>');
+		newImage.attr('src', giphyUrl.images.original_still.url);
+		newImage.click(function(){playPause(this)});
+
+		var ratingText = $('<p>');
+		ratingText.text("Rating: " + giphyUrl.rating);
+
+		newContainer.append(ratingText).append(newImage);
+
+		$('#results').append(newContainer);
 	}
 
-	function playPause(){
-
+	function playPause(curImg){
+		var curPath = $(curImg).attr('src');
+		if (curPath.indexOf("_s.gif") !== -1) {	
+			var toggledPath = curPath.replace("_s.gif", ".gif");
+		} 
+		else {
+			var toggledPath = curPath.replace(".gif", "_s.gif");
+		}
+		$(curImg).attr('src', toggledPath);
 	}
 
+	
 })
